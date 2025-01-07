cd6d6815356e51d55b08b53c2c9acf1c4158d49e
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-01-07 21:26:52 +0200

    Testing how preferences work

--- src/search-tasks.tsx
+++ src/search-tasks.tsx
@@ -1,32 +1,14 @@
-import { ActionPanel, Action, Icon, List } from "@raycast/api";
-
-const ITEMS = Array.from(Array(3).keys()).map((key) => {
-  return {
-    id: key,
-    icon: Icon.Bird,
-    title: "Title " + key,
-    subtitle: "Subtitle",
-    accessory: "Accessory",
-  };
-});
+import { getPreferenceValues } from "@raycast/api";
 
 export default function Command() {
+  const preferences = getPreferenceValues();
+  const organizationName = preferences.organizationName;
+  const personalAccessToken = preferences.personalAccessToken;
+
   return (
-    <List>
-      {ITEMS.map((item) => (
-        <List.Item
-          key={item.id}
-          icon={item.icon}
-          title={item.title}
-          subtitle={item.subtitle}
-          accessories={[{ icon: Icon.Text, text: item.accessory }]}
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard content={item.title} />
-            </ActionPanel>
-          }
-        />
-      ))}
-    </List>
+    <div>
+      <p>Organization Name: {organizationName}</p>
+      <p>Personal Access Token: {personalAccessToken}</p>
+    </div>
   );
 }
