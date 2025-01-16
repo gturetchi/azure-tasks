5c6db70a8539b935d13012679d60c0979eeeaf5b
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-01-16 23:37:25 +0200

    Added metadata details for the task

--- src/features/tasks/TaskDetails.tsx
+++ src/features/tasks/TaskDetails.tsx
@@ -2,5 +2,19 @@ import { Icon, Detail } from "@raycast/api";
 import { WorkItemDetails } from "../../models/task";
 
 export default function TaskDetails({ workItemDetails }: { workItemDetails: WorkItemDetails }) {
-  return <Detail markdown={`# Hello World! \n *** `} navigationTitle={workItemDetails.id.toString()} />;
+  const date = new Date(workItemDetails.fields["System.CreatedDate"]);
+
+  return (
+    <Detail
+      markdown={`# Hello World! \n *** `}
+      navigationTitle={workItemDetails.id.toString()}
+      metadata={
+        <Detail.Metadata>
+          <Detail.Metadata.Label title="State" text={workItemDetails.fields["System.State"]} />
+          <Detail.Metadata.Label title="Assigned to" text={workItemDetails.fields["System.AssignedTo"]} />
+          <Detail.Metadata.Label title="Date created" text={date.toISOString().split("T")[0]} />
+        </Detail.Metadata>
+      }
+    />
+  );
 }
